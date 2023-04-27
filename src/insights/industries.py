2087a53d296ed8c9379c6a056d526844cfc3831a
Alan Foster <alan-af@outlook.com>
2023-04-27 15:43:24 -0300

    req 3

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,17 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    industry_data = read(path)
+    all_indrustries = set()
 
-    Must call `read`
+    for item in industry_data:
+        indrustry = item["industry"]
+        if indrustry != "":
+            all_indrustries.add(indrustry)
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return all_indrustries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
