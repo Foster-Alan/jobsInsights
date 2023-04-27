383a27da45f134b09ae398dac6dcf694cf32c784
Alan Foster <alan-af@outlook.com>
2023-04-27 17:22:05 -0300

    req 11

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+
+    assert count_ocurrences(path, r"javascript") == 122
+    assert count_ocurrences(path, r"python") == 1639
